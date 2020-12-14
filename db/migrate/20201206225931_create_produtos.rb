a3b6ac4cc86dfefd209919d975de579b88b36887
Samuel <samuel.flavio@usp.br>
2020-12-14 12:41:44 -0300

    Create 20201206225931_create_produtos.rb

---
+++ db/migrate/20201206225931_create_produtos.rb
@@ -0,0 +1,13 @@
+class CreateProdutos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :produtos do |t|
+      t.string :nome
+      t.text :descricao
+      t.decimal :preco
+      t.integer :quantidade
+      t.string :categoria
+
+      t.timestamps
+    end
+  end
+end
